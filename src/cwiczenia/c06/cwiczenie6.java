3bf9e1531dc7e4d9496c0a49919664f63627f123
g0at1 <michal.lendzion@vp.pl>
2023-04-11 19:49:21 +0200

    c06

---
+++ src/cwiczenia/c06/cwiczenie6.java
@@ -0,0 +1,117 @@
+package cwiczenia.c06;
+
+import java.util.Scanner;
+
+
+public class cwiczenie6 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        //ZADANIE 2
+        /* 
+        boolean czyPada = true;
+        if (czyPada)
+            System.out.println("Pada deszcz");
+        else
+            System.out.println("Nie pada");
+        */
+
+        //ZADANIE 3
+        /* 
+        boolean czyPada = true;
+        int x = 0;
+
+        if (czyPada == true)
+            x = 5;
+        else 
+            x = 8;
+        System.out.println(x);
+        */
+
+        //ZADANIE 4
+        /* 
+        boolean czyPada = false; 
+        boolean czySwieciSlonce = false;
+
+        if (czyPada == true && czySwieciSlonce == false)
+            System.out.println("Plucha");
+        else if (czyPada == true && czySwieciSlonce == true)
+            System.out.println("Tęcza");
+        else if (czyPada == false && czySwieciSlonce == true)
+            System.out.println("Słonecznie");
+        else 
+            System.out.println("Pochmurno");
+        */
+
+        
+        //ZADANIE 5
+        /* 
+        if (3 < 5 * 2.0)
+        System.out.print("Hello");
+        System.out.print("PPJ");
+        */
+
+        //ZADANIE 6
+        /* 
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int result;
+
+        if (x > 0 && y > 0) {
+            result = x - y;
+            System.out.println(result);
+        }
+        else {
+            result = x + y;
+            System.out.println(result);
+        }
+        */
+
+        
+        //ZADANIE 7
+        /* 
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if ( x % y == 0)
+            System.out.println("Jest wielokrotnoscia");
+        else 
+            System.out.println("Nie jest wielokrotnością");
+        */
+
+        //ZADANIE 8
+        /* 
+        double a = Math.random();
+        double b = Math.random();
+        double c = Math.random();
+
+        if (a > 0.2 && b > 0.2 && c > 0.2)
+            System.out.println("Wieksze");
+        else 
+            System.out.println("Mniejsze");
+        a = Math.random();
+        if (a > 0.2 && b > 0.2 && c > 0.2)
+            System.out.println("Wieksze");
+        else 
+            System.out.println("Mniejsze");
+        b = Math.random();
+        if (a > 0.2 && b > 0.2 && c > 0.2)
+            System.out.println("Wieksze");
+        else 
+            System.out.println("Mniejsze");
+        */
+        
+        
+        
+        
+
+        
+        
+
+
+
+
+
+    }
+}
