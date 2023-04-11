513016df747b762b56774e989fb2eac93071f3c0
g0at1 <michal.lendzion@vp.pl>
2023-04-11 19:50:00 +0200

    c03

---
+++ src/cwiczenia/c03/Main.java
@@ -0,0 +1,29 @@
+package cwiczenia.c03;
+
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+        
+        System.out.println(true);
+        System.out.println(false);
+        
+        System.out.println(13); //dziesietny
+        System.out.println(0xD); //szesnatkowy
+        System.out.println(015); //osemkowy
+        System.out.println(0b1101); //binarny
+
+        System.out.println(1.3 + 0.5 + .5);
+
+        System.out.println('A');
+
+        String weather = "Dzisiaj jest słonecznie";
+
+        System.out.println(weather);
+
+        System.out.println(3.14f);
+
+        long a = 200000000000L;
+        System.out.println(a);
+
+    }
+}
