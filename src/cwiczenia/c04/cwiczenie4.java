f696a12e24c86317234dcd4ee4115f03e9340d39
g0at1 <michal.lendzion@vp.pl>
2023-04-11 19:49:44 +0200

    c04

---
+++ src/cwiczenia/c04/cwiczenie4.java
@@ -0,0 +1,93 @@
+package cwiczenia.c04;
+
+public class cwiczenie4 {
+    public static void main(String[] args) {
+        
+        /* 
+        System.out.println(2 + 2); //4
+        System.out.println(2 - 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 % 2 + "\n");
+        
+        System.out.println(3.14 + 5.94); //9.08
+        System.out.println(3.14 - 5.94);
+        System.out.println(3.14 * 5.94);
+        System.out.println(3.14 / 5.94);
+        System.out.println(3.14 % 5.94 + "\n");
+        
+        System.out.println('A' + 'B'); //131
+        System.out.println('C' - 'D');
+        System.out.println('E' * 'F');
+        System.out.println('G' / 'H');
+        System.out.println('I' / 'J');
+        System.out.println('K' % 'L' + "\n");
+
+        System.out.println('A'); // A
+        System.out.println('A' + 0 + "\n"); // 65 + 0 = 65
+
+        System.out.println('!' + '!'); // 33 + 33
+        System.out.println((char)('!' + '!') + "\n"); // 33 + 33 = 66, a 66 = B
+
+        System.out.println(4 / 3);
+        */
+
+        /*
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        int d = 20;
+
+        System.out.println(d > a && c > b); //true true 
+        System.out.println(a < b && a > d); //true false
+        System.out.println(d < a && c < b); //false false
+        System.out.println(d < a && a < b); //false true 
+        */
+
+        /* 
+        int a = 168;
+        double b = 16.3;
+        double c = .2;
+        float d = 15.87F;
+        double e = 3D;
+        long f = 3L;
+        boolean g = false;
+        String h = "Hello";
+        char i = 'A';
+        int j = 0b10101;
+        */
+
+        /* 
+        int a = 5;
+        int b = 10;
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        */
+
+        /*
+        String czyParzysta = 4 % 2 == 0 ? "parzysta" : "nieparzysta";
+        System.out.println(czyParzysta);
+        */
+
+
+        /* 
+        String czyNarcystyczna = (1 * 1 * 1) + (5 * 5 * 5) + (3 * 3 * 3) == 153 ? "narcystyczna" : "nienarcystyczna";
+        System.out.println(czyNarcystyczna);
+        */
+
+        // System.out.println((double)(((1 + (2 * 3) + 4) / (5 - 6)) + 78) / (9 - (10/11)));
+
+
+        
+
+        
+        
+
+        
+        
+        
+
+
+
+    }
+}
