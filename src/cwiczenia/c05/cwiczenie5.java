f12a67d57ecafed0a9c7d2e8cf250c267ea79a0c
g0at1 <michal.lendzion@vp.pl>
2023-04-11 19:49:34 +0200

    c05

---
+++ src/cwiczenia/c05/cwiczenie5.java
@@ -0,0 +1,68 @@
+package cwiczenia.c05;
+
+import java.lang.Math;
+
+import java.util.Scanner;
+
+public class cwiczenie5 {
+	public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        //ZADANIE 1
+		/*
+		System.out.println(0x26630);
+		System.out.println(0b10_0110_0110_0011_0000);
+        */
+        
+        
+        //ZADANIE 3
+        /* 
+        double sqTwo = Math.sqrt(2);
+        double result = Math.pow(sqTwo, sqTwo) - 2;
+        System.out.println(result);
+
+        String rowne = result == 0 ? "rowne" : "nierowne";
+        System.out.println(rowne);
+        */
+        
+
+        //ZADANIE 4
+        /* 
+        int x = 5;
+        System.out.println((x = 4) * x);
+        */
+        
+        
+
+         
+        
+        //ZADANIE 5
+        /*
+        System.out.println("Wprowadz temperature w F: ");
+        double f = scanner.nextDouble();
+
+        double c = (((f - 32) * 5) / 9);
+        System.out.println("Temperatura w C: " + c);
+        */
+
+
+        //ZADANIE 6
+        /* 
+        System.out.println("Wprowadz rok: ");
+        int x = scanner.nextInt();
+
+        String check = x % 4 == 0 && x % 100 != 0 ? " jest przestepny" : x % 400 == 0 ?  " jest przestepny" : " nie jest przestepny";
+        System.out.println("Rok " + x + check);
+        */
+
+        
+        
+        
+
+        
+
+
+	}
+}
